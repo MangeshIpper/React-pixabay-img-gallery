afd754159c53f3baadb3ab7c2722ab674c13fd10
MangeshIpperNCI <mangesh.ipper@gmail.com>
2022-03-24 00:21:21 +0000

    Added initial commit and  gallery page.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Gallery from './page/Gallery';
+
+function App() {
+  return (
+    <>
+      <h1>Image Gallery</h1>
+      <Gallery />
+    </>
+  );
+}
+
+export default App;
